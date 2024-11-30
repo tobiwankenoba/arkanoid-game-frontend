6e57fd09554818448107f49e58248e5a1491e582
artempudovkin <apudovkin@i-neti.ru>
2024-11-30 17:09:27 +0500

    fix: исправить ошибки линтера

--- packages/client/src/components/Layout/Layout.tsx
+++ packages/client/src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import { Header } from '@/components'
+import { Header } from '@/components/Header'
 
 export const Layout = () => {
   return (
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       '@/pages': path.resolve(__dirname, './src/pages'),
       '@/store': path.resolve(__dirname, './src/store'),
       '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
     },
   },
 })
