234d2ebe596591333d043f67148db892dbd7b0b7
artempudovkin <apudovkin@i-neti.ru>
2024-12-12 12:25:04 +0300

    Revert "feat: сверстать стартовую страницу сайта"
    This reverts commit a67bb535026d4125dd490ae4b5142d5298d33af2.

---
+++ packages/client/src/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import App from './App'
+
+const appContent = 'Home Page!'
+
+// @ts-ignore
+global.fetch = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('hey') })
+)
+
+test('Example test', async () => {
+  render(<App />)
+  expect(screen.getByText(appContent)).toBeDefined()
+})
