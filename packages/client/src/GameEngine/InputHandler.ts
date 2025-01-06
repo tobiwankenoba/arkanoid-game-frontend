d59815e08c29511a6878bfb41fa818a0362fa373
Adam Bersanukaev <adambers2020@gmail.com>
2025-01-06 12:19:41 +0300

    Добавил GamePadapi

--- packages/client/src/GameEngine/InputHandler.ts
+++ packages/client/src/GameEngine/InputHandler.ts
@@ -22,6 +22,42 @@ function inputHandler(game: Game, ball: Ball) {
       game.keyBoardKeys.clear()
     }
   })
+
+  window.addEventListener('gamepadconnected', (e: GamepadEvent) => {
+    console.log('Gamepad connected:', e.gamepad)
+  })
+
+  window.addEventListener('gamepaddisconnected', (e: GamepadEvent) => {
+    console.log('Gamepad disconnected:', e.gamepad)
+  })
+
+  function updateGamepad() {
+    const gamepads = navigator.getGamepads ? navigator.getGamepads() : []
+    if (gamepads) {
+      const gamepad = gamepads[0]
+      if (gamepad) {
+        if (gamepad.buttons[14].pressed) {
+          game.keyBoardKeys.add('ArrowRight')
+          if (gamepad.buttons[15].pressed) {
+            game.keyBoardKeys.add('ArrowLeft')
+          }
+          const leftStickX = gamepad.axes[0]
+          if (leftStickX < -0.5) {
+            ball.move()
+          } else if (leftStickX > 0.5) {
+            ball.move()
+          }
+        }
+      }
+    }
+
+    function gameLoop() {
+      updateGamepad()
+      requestAnimationFrame(gameLoop)
+    }
+
+    gameLoop()
+  }
 }
 
 export default inputHandler
