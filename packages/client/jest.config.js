695233e46a1d48818ab0611112af6cb81284194e
Adam Bersanukaev <adambers2020@gmail.com>
2025-01-19 11:31:47 +0300

    add tests

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -19,7 +19,8 @@ export default {
     '^@/assets/(.*)$': '<rootDir>/src/assets/$1',
   },
   transform: {
-    "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/file.parser.cjs",
-  }
-};
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/file.parser.cjs',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}
---
+++ packages/client/jest.setup.js
@@ -0,0 +1,2 @@
+const { createCanvas } = require('canvas')
+global.HTMLCanvasElement = createCanvas
